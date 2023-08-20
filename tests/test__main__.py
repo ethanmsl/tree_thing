bbd6493bf493b5c3a6ba710a392f03faed7a5b90
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-08-20 17:16:05 +0100

    Initial commit

---
+++ tests/test__main__.py
@@ -0,0 +1,9 @@
+"""
+May not be anything to test here as `__main__.py` is typically just an entry point
+into the program
+"""
+
+
+def test_truisim():
+    """Test: True is True"""
+    assert True
